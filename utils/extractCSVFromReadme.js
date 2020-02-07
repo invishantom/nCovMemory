a7701a70752e2ae5cbe94c39841ca3aa29ea10e2
JohnnRen <34511166+JohnnRen@users.noreply.github.com>
2020-02-07 10:49:59 +0800

    feat: update utils to extract the media

--- utils/extractCSVFromReadme.js
+++ utils/extractCSVFromReadme.js
@@ -7,12 +7,15 @@ fs.readFile('../README.md', 'utf8', function(err, data) {
   var match = regex.exec(data);
   let extracted = [];
   while ((match = regex.exec(data)) != null) {
-    extracted.push([match[1], match[2], match[3], match[4]]);
+    let indexHeader = data.slice(0, regex.lastIndex).lastIndexOf('###');
+    let indexLinebreak = data.indexOf('\n', indexHeader);
+    let media = data.slice(indexHeader + 4, indexLinebreak - 1);
+    extracted.push([media, match[1], match[2], match[3], match[4]]);
   }
   var csv = convertArrayToCSV(extracted, {
-    header: ['date', 'title', 'url', 'archive', 'screenshot'],
+    header: ['media', 'date', 'title', 'url', 'archive', 'screenshot'],
     separator: ','
   });
-  console.log(csv);
-  fs.writeFileSync('./data.csv', csv, 'utf-8');
+  // console.log(csv);
+  fs.writeFileSync('../data/non-fiction.csv', csv, 'utf-8');
 });
