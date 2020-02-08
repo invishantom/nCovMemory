e51919763617c56f5ca3e69b17c9dde9aeb663b3
John <2019ncovmemory@gmail.com>
2020-02-08 22:40:45 +0800

    fix: convert header links to lowercase

--- utils/generateReadmeFromCsv.js
+++ utils/generateReadmeFromCsv.js
@@ -14,7 +14,7 @@ Papa.parsePromise = function(file, options) {
   });
 };
 Handlebars.registerHelper('encode', function(string) {
-  return (string = string.replace(/[\(|\)|（|）]/g, ''));
+  return (string = string.replace(/[\(|\)|（|）]/g, '').toLowerCase());
 });
 Handlebars.registerHelper('index', function(string) {
   return (string = string.replace('（个体）', ''));
