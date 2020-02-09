8b1db1c8d670094a20ef2934adc8395c775b2ec3
John <2019ncovmemory@gmail.com>
2020-02-10 02:15:28 +0800

    fix: fix index not reading directories

--- archive/index.js
+++ archive/index.js
@@ -4,9 +4,12 @@ const PUBLIC_PATH =
   'https://github.com/2019ncovmemory/nCovMemory/tree/features/picture-compression/archive';
 const fs = require('fs');
 let index = {};
-
-const isDirectory = (source) => fs.lstatSync(source).isDirectory();
-let cats = fs.readdirSync(__dirname).filter((cat) => isDirectory(cat));
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+let cats = getDirectories(__dirname);
 for (cat of cats) {
   fs.readdirSync(path.join(__dirname, cat)).forEach((name) => {
     let id = name.replace(/\.\w*$/, '');
