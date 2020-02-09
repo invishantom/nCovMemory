8b2f4a7d185547428e0f05b81e6e93a7efc76f3a
John <2019ncovmemory@gmail.com>
2020-02-09 19:53:16 +0800

    feat: manage archive with id

---
+++ archive/index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+const PUBLIC_PATH = 'https://github.com/2019ncovmemory/nCovMemory/blob/master';
+const fs = require('fs');
+let index = {};
+
+let cats = fs.readdirSync(__dirname).filter((cat) => cat !== 'index.js');
+for (cat of cats) {
+  fs.readdirSync(path.join(__dirname, cat)).forEach((name) => {
+    let id = name.replace(/\.\w*$/, '');
+    index.hasOwnProperty(id) || (index[id] = {});
+    index[id][cat] = `${PUBLIC_PATH}/${cat}/${name}`;
+  });
+}
+// console.log(index);
+
+module.exports = index;
--- utils/generateReadmeFromCsv.js
+++ utils/generateReadmeFromCsv.js
@@ -1,83 +1,85 @@
+const path = require('path');
 const Papa = require('papaparse');
-// const Mustache = require('Mustache');
 const Handlebars = require('handlebars');
 const { differenceInDays, parse, compareDesc } = require('date-fns');
-const path = require('path');
-const README_PATH = path.join(__dirname, '..', 'README.md');
-const CSV_PATHS = require('../data/index');
 var querystring = require('querystring');
-var fs = require('fs');
+var fs = require('fs-extra');
+const ARCHIVE = require('../archive/index');
+const README_PATH = path.join(__dirname, '..', 'README.md');
+const DATA = require('../data/index');
 
 Papa.parsePromise = function(file, options) {
   return new Promise(function(complete, error) {
     Papa.parse(file, { complete, error, ...options });
   });
 };
-Handlebars.registerHelper('encode', function(string) {
+
+Handlebars.registerHelper('link', function(string) {
   // return encodeURIComponent(string);
   return (string = string.replace(/[\(|\)|（|）|<|>|《|》|【|】|\[|\]|、|\/]/g, '').toLowerCase());
 });
+Handlebars.registerHelper('table', function(string) {
+  return (string = string.replace(/\|/g, '\\|'));
+});
+
 Handlebars.registerHelper('index', function(string) {
   return (string = string.replace('（个体）', ''));
 });
+
 function renameKey(o, old_key, new_key) {
   if (old_key !== new_key) {
     Object.defineProperty(o, new_key, Object.getOwnPropertyDescriptor(o, old_key));
     delete o[old_key];
   }
 }
-async function parseData(csvPath) {
+
+async function generate() {
+  let model = {
+    narrative: { articles: {}, medias: [] },
+    non_fiction: { articles: {}, medias: [] }
+  };
   let now = new Date();
-  let csv = fs.readFileSync(csvPath, 'utf8');
-  let data = await Papa.parsePromise(csv, { header: true });
-  data.data = data.data.filter(
-    (entry) => entry.title && entry.media && entry.date && entry.update && entry.url
+  let csv = fs.readFileSync(DATA['data'].path, 'utf8');
+  // Read csv
+  let { data } = await Papa.parsePromise(csv, { header: true });
+  data = data.filter(
+    (entry) => entry.id && entry.title && entry.media && entry.date && entry.update && entry.url
   );
-  let medias = [];
-  for (let entry of data.data) {
+  // Extract medias and articles
+  for (let entry of data) {
+    // Calculate attributes
     entry.is_new = differenceInDays(now, parse(entry.update, 'MM-dd', new Date())) <= 1;
     entry.is_deleted = entry.is_deleted === 'true' || entry.is_deleted === 'TRUE';
-    entry.screenshot = entry.screenshot
-      ? `https://github.com/2019ncovmemory/nCovMemory/blob/master/archive/png/${entry.screenshot}.png`
-      : null;
-    entry.title = entry.title.replace('|', '\\|');
-
-    if (medias.indexOf(entry.media) === -1) {
-      medias.push(entry.media);
+    if (ARCHIVE.hasOwnProperty(entry.id)) {
+      entry = { ...ARCHIVE[entry.id], ...entry };
+    }
+    if (entry.category === 'narrative' && model['non_fiction'].medias.indexOf(entry.media) !== -1) {
+      entry.media = `${entry.media}（个体）`;
+    }
+    if (model[entry.category].medias.indexOf(entry.media) === -1) {
+      model[entry.category].medias.push(entry.media);
+    }
+    if (!model[entry.category].articles[entry.media]) {
+      model[entry.category].articles[entry.media] = [];
+    }
+    model[entry.category].articles[entry.media].push(entry);
+  }
+  // Sort
+  for (let cat in model) {
+    model[cat].medias.sort(function compareFunction(param1, param2) {
+      return param1.localeCompare(param2, 'zh');
+    });
+    for (let media in model[cat].articles) {
+      model[cat].articles[media].sort((a, b) =>
+        compareDesc(parse(a.date, 'MM-dd', new Date()), parse(b.date, 'MM-dd', new Date()))
+      );
     }
   }
-  medias.sort(function compareFunction(param1, param2) {
-    return param1.localeCompare(param2, 'zh');
-  });
 
-  let articles = {};
-  for (let media of medias) {
-    articles[media] = data.data.filter((entry) => entry.media === media);
-    articles[media].sort((a, b) =>
-      compareDesc(parse(a.date, 'MM-dd', new Date()), parse(b.date, 'MM-dd', new Date()))
-    );
-  }
-  return { medias, articles };
-}
-async function generate() {
-  let model = {};
-  for (key in CSV_PATHS) {
-    let result = await parseData(CSV_PATHS[key].path);
-    model[key] = {
-      ...result
-    };
-  }
   let template = fs.readFileSync(
     path.join(__dirname, '..', 'template', 'README.handlebars'),
     'utf8'
   );
-  for (let [index, media] of model['narrative'].medias.entries()) {
-    if (model['non_fiction'].medias.indexOf(media) !== -1) {
-      let newName = `${media}（个体）`;
-      renameKey(model['narrative'].articles, media, newName);
-      model['narrative'].medias[index] = newName;
-    }
-  }
   let runtime = Handlebars.compile(template);
   const output = runtime(model);
   fs.writeFileSync(README_PATH, output, 'utf-8');
