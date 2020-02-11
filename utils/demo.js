50d0575cb18d3ea0f8ea2d0d566322aa61dc3db0
John <2019ncovmemory@gmail.com>
2020-02-12 01:12:26 +0800

    feat: update generator (wip)

---
+++ utils/demo.js
@@ -0,0 +1,34 @@
+let data = {
+  narrative: {
+    一个媒体: [
+      //! 该媒体的一篇文章
+      {
+        id: '1',
+        media: '一个媒体',
+        media_en: 'A Media',
+        name: '一个媒体的一篇文章',
+        name_en: 'An Article',
+        update: '00-00',
+        date: '00-00',
+        is_deleted: 'true',
+        url: '原文url',
+        alternative: '转载url',
+        archive: '存档url',
+        translation: '翻译url'
+      },
+
+      //! 另一篇文章
+      {
+        // ...
+      }
+      // ...
+    ],
+
+    另一个媒体: []
+    // ...
+  },
+
+  non_fiction: {
+    // ...
+  }
+};
--- utils/generateReadmeFromCsv.js
+++ utils/generateReadmeFromCsv.js
@@ -4,54 +4,66 @@ const Handlebars = require('handlebars');
 const { differenceInDays, parse, compareDesc } = require('date-fns');
 var querystring = require('querystring');
 var fs = require('fs-extra');
+
 const ARCHIVE = require('../archive/index');
-const README_PATH = path.join(__dirname, '..', 'README.md');
 const DATA = require('../data/index');
+const MEDIA_EN = require('../data/media_en.json');
+const CATEGORY_TEXT = require('../template/category_text.json');
 const ORDERING = require('../template/ordering.json');
+const README_PATH = path.join(__dirname, '..', 'README.md');
 const TEMPLATE_PATH = path.join(__dirname, '..', 'template', 'README.handlebars');
+
+let model = new Map();
+
+function getUniqueValue(arrayOfObject, property) {
+  return arrayOfObject
+    .map((a) => a[property])
+    .filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+}
 
 Papa.parsePromise = function(file, options) {
   return new Promise(function(complete, error) {
     Papa.parse(file, { complete, error, ...options });
   });
 };
-let model = {
-  narrative: { articles: {}, medias: [] },
-  non_fiction: { articles: {}, medias: [] }
-};
 
 // Escape Markdown Link
-Handlebars.registerHelper('link', function(string) {
-  // return encodeURIComponent(string);
+function escapeLink(string) {
   return (string = string.replace(/[\(|\)|（|）|<|>|《|》|【|】|\[|\]|、|\/]/g, '').toLowerCase());
-});
+}
+
+Handlebars.registerHelper('link', escapeLink);
 
 // Escape Markdown Table
-Handlebars.registerHelper('table', function(string) {
+function escapeTable(string) {
   return (string = string.replace(/\|/g, '\\|'));
-});
+}
+
+Handlebars.registerHelper('table', escapeTable);
 
 // Indicate Narratives
-Handlebars.registerHelper('narrative', function(string) {
-  if (Object.keys(model['non_fiction'].articles).indexOf(string) !== -1) {
+function indicateNarrative(string) {
+  if (model.get('non_fiction').has(string)) {
     string = `${string}（个体）`;
   }
   return string;
-});
+}
+
+Handlebars.registerHelper('narrative', indicateNarrative);
 
 Handlebars.registerHelper('narrative_link', function(string) {
-  if (Object.keys(model['non_fiction'].articles).indexOf(string) !== -1) {
-    string = `${string}（个体）`;
-  }
-  return (string = string.replace(/[\(|\)|（|）|<|>|《|》|【|】|\[|\]|、|\/]/g, '').toLowerCase());
+  return escapeLink(indicateNarrative(string));
 });
 
-// function renameKey(o, old_key, new_key) {
-//   if (old_key !== new_key) {
-//     Object.defineProperty(o, new_key, Object.getOwnPropertyDescriptor(o, old_key));
-//     delete o[old_key];
-//   }
-// }
+function trimAttributes(object, attributes) {
+  attributes.forEach((a) => {
+    if (object.hasOwnProperty(a)) {
+      a = a.trim();
+    }
+  });
+}
 
 async function generate() {
   let now = new Date();
@@ -62,55 +74,47 @@ async function generate() {
     (entry) =>
       entry.id && entry.category && entry.title && entry.media && entry.date && entry.update
   );
-  // Extract medias and articles
-  for (let entry of data) {
-    // Calculate attributes
+
+  data.forEach((entry) => {
     entry.title = entry.title.trim();
+    entry.title_en = entry.title_en.trim();
+
     entry.media = entry.media.trim();
     entry.is_new = differenceInDays(now, parse(entry.update, 'MM-dd', new Date())) <= 1;
     entry.is_deleted = entry.is_deleted === 'true' || entry.is_deleted === 'TRUE';
     if (ARCHIVE.hasOwnProperty(entry.id)) {
       entry = { ...ARCHIVE[entry.id], ...entry };
     }
-    if (!model[entry.category].articles[entry.media]) {
-      model[entry.category].articles[entry.media] = [];
-    }
-    model[entry.category].articles[entry.media].push(entry);
-  }
+  });
 
-  // Sort
-  for (let cat in model) {
-    let orderedArticles = {};
-    for (let expectedMedia of ORDERING[cat]) {
-      console.log(`Generating ${expectedMedia} ...`);
-      orderedArticles[expectedMedia] = model[cat].articles[expectedMedia];
-      delete model[cat].articles[expectedMedia];
-    }
-    orderedArticles = Object.assign(model[cat].articles, orderedArticles);
-    for (let media in orderedArticles) {
-      orderedArticles[media].sort((a, b) =>
-        compareDesc(parse(a.date, 'MM-dd', new Date()), parse(b.date, 'MM-dd', new Date()))
-      );
-    }
-    model[cat].articles = orderedArticles;
-    model[cat].medias = Object.keys(model[cat].articles);
-    // Sort by name, no longer used
-    // model[cat].medias.sort(function compareFunction(param1, param2) {
-    //   return param1.localeCompare(param2, 'zh');
-    // });
-  }
-
-  // for (media in model['narrative'].articles) {
-  //   if (Object.keys(model['non_fiction'].articles).indexOf(media) !== -1) {
-  //     renameKey(model['narrative'].articles, media, `${media}（个体）`);
-  //   }
-  // }
-  model['narrative'].medias = Object.keys(model['narrative'].articles);
+  let cats = getUniqueValue(data, 'category');
+  cats.forEach((cat) => {
+    let articlesOfCat = data.filter((d) => d.category === cat);
+    let mediasOfCat = getUniqueValue(articlesOfCat, 'media');
+    mediasOfCat.sort((a, b) => {
+      let expectedOrderA = ORDERING[cat].indexOf(a);
+      let expectedOrderB = ORDERING[cat].indexOf(b);
+      return expectedOrderB - expectedOrderA;
+    });
+    let articlesByMedia = new Map();
+    mediasOfCat.forEach((media) => {
+      console.log(`Generating ${media} ...`);
+      let articlesOfMedia = articlesOfCat
+        .filter((d) => d.media === media)
+        .sort((a, b) =>
+          compareDesc(parse(a.date, 'MM-dd', new Date()), parse(b.date, 'MM-dd', new Date()))
+        );
+      articlesByMedia.set(media, articlesOfMedia);
+    });
+    model.set(CATEGORY_TEXT[cat], articlesByMedia);
+  });
 
   let template = fs.readFileSync(TEMPLATE_PATH, 'utf8');
   let runtime = Handlebars.compile(template);
-  const output = runtime(model);
-  fs.writeFileSync(README_PATH, output, 'utf-8');
+  console.log(model);
+
+  // // const output = runtime(model);
+  // fs.writeFileSync(README_PATH, output, 'utf-8');
   console.log('Generate README succeed!');
 }
 
